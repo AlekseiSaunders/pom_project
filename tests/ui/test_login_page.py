a63fe5c6c781e955dec5d28e21be02159db61f6c
Alex Saunders <alex.saunders@wildlifeprotectionsolutions.org>
2024-08-12 19:02:10 -0600

    Updated test_login_page to use setup/teardown

--- tests/ui/test_login_page.py
+++ tests/ui/test_login_page.py
@@ -20,19 +20,19 @@ admin_user = os.getenv("ADMIN_USERNAME")
 admin_pass = os.getenv("ADMIN_PASSWORD")
 fail_user = fake.email()
 fail_pass = fake.password()
+
+@pytest.mark.usefixtures("setup_isolated")
 class TestLogin:
     
     @pytest.mark.run(order=1)
     @pytest.mark.fails
-    def test_invalid_login(self):
-        logger.info("Initializing driver and maximizing window")
-        self.driver = setup_webdriver('chrome')
-        self.driver.maximize_window()
-        self.wait = WebDriverWait(self.driver, DEFAULT_TIMEOUT)
-        self.driver.get("https://www.letskodeit.com/")
+    def test_invalid_login(self, setup):
+        driver,wait = setup
         
-        lp = LoginPage(self.driver)
-        logger.info("Attempting to login from invalid test")
+        logger.info(f"Attempting to login from invalid test on {driver.name}")
+        driver.get("https://www.letskodeit.com/")
+        
+        lp = LoginPage(driver)
         lp.login(fail_user, fail_pass)
         login_failed = lp.verify_login_failed()
         assert login_failed, "Login was successful"
@@ -40,15 +40,12 @@ class TestLogin:
         
     @pytest.mark.run(order=2)
     @pytest.mark.succeeds
-    def test_valid_login(self):
-        logger.info("Initializing driver and maximizing window.")
-        self.driver = setup_webdriver('chrome')
-        self.driver.maximize_window()
-        self.wait = WebDriverWait(self.driver, DEFAULT_TIMEOUT)
-        self.driver.get("https://www.letskodeit.com/")
+    def test_valid_login(self, setup):
+        driver, wait = setup
+        logger.info(f"Attempting to login from valid test on {driver.name}")
+        driver.get("https://www.letskodeit.com/")
     
-        lp = LoginPage(self.driver)
-        logger.info("Attempting to login from valid test")
+        lp = LoginPage(driver)
         lp.login(admin_user, admin_pass)
         login_successful =  lp.verify_login_successful()
         assert login_successful, "Login was not successful"
@@ -56,18 +53,18 @@ class TestLogin:
             
     @pytest.mark.run(order=3)
     @pytest.mark.succeeds
-    def test_all_elements(self):
-        logger.info("Initializing driver and maximizing window.")
-        self.driver = setup_webdriver('chrome')
-        self.driver.maximize_window()
-        self.wait = WebDriverWait(self.driver, DEFAULT_TIMEOUT)
-        self.driver.get("https://www.letskodeit.com/")
-        
-        lp = LoginPage(self.driver)
+    def test_all_elements(self, setup):
+        driver, wait = setup
+
+        logger.info(f"Verifying all elements are present on {driver.name}")
+        driver.get("https://www.letskodeit.com/")
+
+        lp = LoginPage(driver)
         lp.verify_all_elements_present()
             
 
 if __name__ == "__main__":
     TL = TestLogin()
+    TL.test_invalid_login()
     TL.test_valid_login()
     TL.test_all_elements()
